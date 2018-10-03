8ddb79643f7c35c2068b0a70c2c71bd482a940a7
Hannah O'Sullivan <hjosullivan123@gmail.com>
2018-10-03 11:30:22 +0100

    First commit

---
+++ Week1/Code/boilerplate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Author: Hannah O'Sullivan h.osullivan18@imperial.ac.uk
+# Script: boilerplate.sh
+# Desc: simple boilerplate for shell scripts
+# Arguments: none
+# Date: Oct 2018
+
+echo -e "\nThis is a shell script! \n" #what does -e do?
+#The -e option is used to enable echo's interpretation
+#of additional instances of the newline character as
+#well as the interpretation of other special characters,
+# such as a horizontal tab, which is represented by \t.
+
+#exit
