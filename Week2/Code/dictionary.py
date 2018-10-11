edca02e00184342453012b4d7e0cf8a4b51aa016
Hannah O'Sullivan <hjosullivan123@gmail.com>
2018-10-11 15:08:12 +0100

    Add dictionary.py

--- Week2/Code/dictionary.py
+++ Week2/Code/dictionary.py
@@ -10,13 +10,22 @@ taxa = [ ('Myotis lucifugus','Chiroptera'),
          ('Canis lupus', 'Carnivora'),
         ]
 
-# Write a short python script to populate a dictionary called taxa_dic 
-# derived from  taxa so that it maps order names to sets of taxa. 
-# E.g. 'Chiroptera' : set(['Myotis lucifugus']) etc. 
+# Write a short python script to populate a dictionary called taxa_dic
+# derived from  taxa so that it maps order names to sets of taxa.
+# E.g. 'Chiroptera' : set(['Myotis lucifugus']) etc.
 
-# ANNOTATE WHAT EVERY BLOCK OR IF NECESSARY, LINE IS DOING! 
+# ANNOTATE WHAT EVERY BLOCK OR IF NECESSARY, LINE IS DOING!
 
-# ALSO, PLEASE INCLUDE A DOCSTRING AT THE BEGINNING OF THIS FILE THAT 
+# ALSO, PLEASE INCLUDE A DOCSTRING AT THE BEGINNING OF THIS FILE THAT
 # SAYS WHAT THE SCRIPT DOES AND WHO THE AUTHOR IS
 
-# Write your script here:
+#Write your script here:
+
+taxa_dic = {} #create an empty dictionary
+for i in taxa: #for order
+    if i[1] in taxa_dic.keys(): #make order a key
+        taxa_dic[i[1]].append(i[0]) #and append species
+    else:
+        taxa_dic[i[1]] = [i[0]] #if it is not already a key, make new one and populate
+
+print(taxa_dic)
