01a1703e4cffc0d9dacd9332eaa94450781b8d98
Hannah O'Sullivan <hjosullivan123@gmail.com>
2018-10-15 14:36:21 +0100

    Add basic_io.R - example input and output in R

---
+++ Week3/Code/basic_io.R
@@ -0,0 +1,20 @@
+#A simple script to illustrate R input and output.
+#Run line by line and check inputs and outputs to understand what is happening
+
+#clean environment
+rm(list = ls())
+
+#import with headers
+MyData <- read.csv("../Data/trees.csv", header = TRUE)
+
+#write it out as a new file
+write.csv(MyData, "../Results/MyData.csv")
+
+#append to this
+write.table(MyData[1,], file = "../Results/MyData.csv",append=TRUE)
+
+#write row names
+write.csv(MyData,"../Results/MyData.csv", row.names = TRUE)
+
+#ignore column names
+write.table(MyData, "../Results/MyData.csv", col.names = FALSE)
