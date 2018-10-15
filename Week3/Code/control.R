2103bbf4ce294487e75d8f8583fb46830e79ea1e
Hannah O'Sullivan <hjosullivan123@gmail.com>
2018-10-15 15:01:51 +0100

    Add control.R

---
+++ Week3/Code/control.R
@@ -0,0 +1,50 @@
+#########################################################
+## Some code exemplifying control flow constructs in R ##
+#########################################################
+
+#clear environemnt
+rm(list = ls())
+
+#if statement
+a <- TRUE
+if (a == TRUE) {
+  print("a is TRUE")
+} else {
+  print("a is FALSE")
+}
+
+#on a single line
+z <- runif(1) #a random number
+if (z <= 0.5) {
+  print("Les than a quarter")
+}
+
+#for loop using a sequence
+for(i in 1:1000){
+  j <- i * i
+  print(paste(i, " squared is", j))
+}
+
+#for loop over vector of strings
+for(species in c("Heliodoxa rubinoides",
+                 "Boissonneaua jardini",
+                 "Sula nebouxii"))
+{
+  print(paste("The species is", species))
+}
+
+#for loop using a vector
+v1 <- c("a", "bc", "def")
+for(i in v1){
+  print(i)
+}
+
+#while loop
+i <- 0
+while(i < 100){
+  i <- i + 1
+  print(i ^ 2)
+}
+
+
+
