75baa4dd290f501f60f8aa7c0991eb615fb9b8a0
Hannah O'Sullivan <hjosullivan123@gmail.com>
2018-10-15 15:14:55 +0100

    Add break.R

---
+++ Week3/Code/break.R
@@ -0,0 +1,20 @@
+###########################
+## Breaking out of loops ##
+###########################
+
+#sometimes it is useful to break out of a loop when some condition is met.
+#use break in situations when you cannot set a target number of iterations.
+
+#clear environment
+rm(list = ls())
+
+i <- 0 #initialize i
+    while(i < Inf){
+      if (i == 20){
+        break} #break freeeeee
+      else {
+        cat("i equals ", i, " \n"
+)
+        i <- i + 1 #update i
+      }
+    }
