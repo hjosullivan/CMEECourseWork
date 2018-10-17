a0c729e101e5de0f7b7b47c29689c934ee4a741f
Hannah O'Sullivan <hjosullivan123@gmail.com>
2018-10-17 12:11:04 +0100

    Update using_name.py

--- Week2/Code/using_name.py
+++ Week2/Code/using_name.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+
+A script to clarify the use of __name__ == __main__
+
+"""
+
+__author__ = "Hannah O'Sullivan (h.osullivan18@imperial.ac.uk)"
+__appname__ = "Using name"
+__version__ = "0.0.1"
+__license__ = "I do not have one"
+
+
 if __name__ == '__main__':
     print ('This program is being run by itself')
 else:
