c5f1395141169d4bc7a14c86e5e42c22e802ef89
Hannah O'Sullivan <hjosullivan123@gmail.com>
2018-10-17 15:29:06 +0100

    Update TreeHeight.R to use dplyr package

--- Week3/Code/TreeHeight.R
+++ Week3/Code/TreeHeight.R
@@ -13,15 +13,30 @@
 
 #clear environment
 rm(list = ls())
+#require(dplyr)
 
+#if dplyr is not installed, install it
+if("dplyr" %in% rownames(installed.packages()) == FALSE){
+  install.packages("dplyr")
+}
+
+#import tree csv - use relative path!
+trees <- read.csv("../Data/trees.csv", header = TRUE)
+
+#function for calulating tree height
 TreeHeight <- function(degrees, distance){
   radians <- degrees * pi/180
   height <- distance * tan(radians)
-  print(paste("Tree height is:", height
-  ))
   
   return(height)
   
 }
 
-TreeHeight(37, 40)
+#use dplyr
+test_df <- trees %>% dplyr::group_by(Species) %>% dplyr::mutate(Tree.Height.m = TreeHeight(Angle.degrees, Distance.m))
+#look at the first few rows 
+head(test_df)
+
+#write row names
+write.csv(test_df,"../Results/TreeHts.csv", row.names = TRUE)
+
