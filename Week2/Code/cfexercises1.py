b5e7a846c554a4c8e293e8f0cf02a94df70524fe
Hannah O'Sullivan <hjosullivan123@gmail.com>
2018-10-17 12:42:46 +0100

    Add docstring

--- Week2/Code/cfexercises1.py
+++ Week2/Code/cfexercises1.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
+"""
+
+Control flow exercise 1
+
+"""
+# Docstrings are considered part of the running code
+# (Normal comments are stripped).
+# Hence you can access your docstrings at the run time.
+
+__author__ = 'Hannah O Sullivan (h.osullivan18@imperial.ac.uk)'
+__appname__ = 'Control flow exercise 1'
+__version__ = '0.0.1'
+__license__ = "I do not have one"
+
 for i in range (3, 17):
     print("hello")
 
