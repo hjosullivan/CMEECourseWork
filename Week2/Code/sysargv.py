4bb8ebad41d4bb759845aa3cfdb9026301a7364f
Hannah O'Sullivan <hjosullivan123@gmail.com>
2018-10-17 12:32:04 +0100

    Update sysargv.py

--- Week2/Code/sysargv.py
+++ Week2/Code/sysargv.py
@@ -1,4 +1,15 @@
-#!/user/bin/env python3
+#!/usr/bin/env python3
+
+"""
+
+Example system arguments
+
+"""
+
+__author__ = "Hannah O'Sullivan (h.osullivan18@imperial.ac.uk)"
+__appname__ = "System arguments"
+__version__ = "0.0.1"
+__license__ = "I do not have one"
 
 import sys
 
