71579dd58c5576af5018d9f3e69e270ccf2572f9
Hannah O'Sullivan <hjosullivan123@gmail.com>
2018-10-17 12:03:45 +0100

    Change export path to Results directory

--- Week2/Code/basic_io.py
+++ Week2/Code/basic_io.py
@@ -41,7 +41,7 @@ f.close()
 # Save the elements of a list to a file
 list_to_save = range(100)
 
-f = open('../Sandbox/testout.txt', 'w')
+f = open('../Results/testout.txt', 'w')
 for i in list_to_save:
     f.write(str(i) + '\n') # Add new line at the end
 
@@ -55,13 +55,13 @@ f.close()
 my_dictionary = {"a key": 10, "another key": 11}
 import pickle
 
-f = open('../Sandbox/testp.p', 'wb')
+f = open('../Results/testp.p', 'wb')
 # note the b: accept binary files
 pickle.dump(my_dictionary, f)
 f.close()
 
 # Load data again
-f = open('../Sandbox/testp.p', 'rb')
+f = open('../Results/testp.p', 'rb')
 another_dictionary = pickle.load(f)
 f.close()
 
