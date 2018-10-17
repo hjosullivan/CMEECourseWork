2bcda873b70e61837f831c6271ecf8855e459aab
Hannah O'Sullivan <hjosullivan123@gmail.com>
2018-10-17 11:44:19 +0100

    Add docstring

--- Week2/Code/tuple.py
+++ Week2/Code/tuple.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+
+""" An exercise exemplifying the use of tuples. """
+
+__author__ = 'Hannah O Sullivan (h.osullivan18@imperial.ac.uk)'
+__appname__ = 'Control flow exercise 2'
+__version__ = '0.0.1'
+__license__ = "I do not have one"
+
+
 birds = ( ('Passerculus sandwichensis','Savannah sparrow',18.7),
           ('Delichon urbica','House martin',19),
           ('Junco phaeonotus','Yellow-eyed junco',19.5),
@@ -10,11 +20,12 @@ birds = ( ('Passerculus sandwichensis','Savannah sparrow',18.7),
 # Hints: use the "print" command! You can use list comprehension!
 #print(birds)
 
-for i in birds:
-    print(i)
-
+#using a loop
 #for i in birds:
-#    print("{} {} {}".format(i[0], i[1], i[-1]))
+#    print(i)
+
+for i in birds:
+    print("{} {} {}".format(i[0], i[1], i[-1]))
 
 
 
