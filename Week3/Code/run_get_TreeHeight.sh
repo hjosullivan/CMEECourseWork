403497cae2d256fbcec2025156cc10e747219acb
Hannah O'Sullivan <hjosullivan123@gmail.com>
2018-10-23 21:44:27 +0100

    Add run_get_TreeHeight.sh

---
+++ Week3/Code/run_get_TreeHeight.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Author: Hannah O'Sullivan h.osullivan18@imperial.ac.uk
+# Script: boilerplate.sh
+# Desc: test get_TreeHeight.R
+# Arguments: none
+# Date: Oct 2018
+
+echo "Running get_TreeHeight.R"
+Rscript get_TreeHeight.R ../Data/trees.csv
+echo "Done!"
