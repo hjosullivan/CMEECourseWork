e9bee060a57d099211b3aee94482eb31783efa3b
Hannah O'Sullivan <hjosullivan123@gmail.com>
2018-10-24 13:22:39 +0100

    Create functions to print system time

--- Week3/Code/preallocate.R
+++ Week3/Code/preallocate.R
@@ -3,27 +3,48 @@
 ####################
 
 ## Author: Hannah O'Sullivan h.osullivan18@imperial.ac.uk
-## Script: boilerplate.R
-## Desc: Introduction to writing R functions
+## Script: preallocate.R
+## Desc: Introduction to preallocation
 ## Date: October 2018
 
 #clean environment
 rm(list = ls())
 
-#function 1 (loop)
+#first
+#a <- NA
+#for (i in 1:100000) {
+#  a <- c(a, i)
+#}
+#print(a)
+
+#function 1
 a <- NA
-for (i in 1:100000) {
-  a <- c(a, i)
-}
+myfunc1 <- function(a) {
+  for(i in 1:100000) {
+    a <- c(a, i)
+  }
 print(a)
-
-
-#function 2 (pre-allocated vector)
-a <- rep(NA, 1000000)
-
-for (i in 1:1000000) {
-  a[i] <- i
 }
-print(a)
 
-system.time()
+myfunc1(a)
+print(system.time(myfunc1(a)))
+
+#second
+#a <- rep(NA, 1000000)
+
+#for (i in 1:1000000) {
+#  a[i] <- i
+#}
+#print(a)
+
+#function 2
+b <- rep(NA, 100000)
+myfunc2 <- function(b) {
+  for (i in 1:100000) {
+    b[i] <- i
+  }
+print(b)
+}
+myfunc2(b)
+print(system.time(myfunc2(b)))
+
