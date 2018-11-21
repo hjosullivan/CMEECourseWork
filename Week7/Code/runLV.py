18caf236665745b2f307c0f4de33c31035137000
Hannah O'Sullivan <hjosullivan123@gmail.com>
2018-11-21 14:04:48 +0000

    Add LV2 profling

--- Week7/Code/runLV.py
+++ Week7/Code/runLV.py
@@ -2,9 +2,10 @@ import cProfile
 import pstats
 from io import StringIO
 import LV1
+import LV2
 
+#profile LV1
 pr = cProfile.Profile()
-
 
 pr.enable()
 LV1.main([])
@@ -16,3 +17,17 @@ ps.print_stats(0).sort_stats("calls") #sort by calls
 
 print("Profiling LV1.py:")
 print(s.getvalue())
+
+#profile LV2
+pr2 = cProfile.Profile()
+
+pr2.enable()
+LV2.main([])
+pr2.disable()
+
+s2 = StringIO()
+ps2 = pstats.Stats(pr2, stream=s2)
+ps2.print_stats(0).sort_stats("calls") #sort by calls
+
+print("Profiling LV1.py:")
+print(s2.getvalue())
