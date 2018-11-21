f4fc980d139bf7b9da676df18eda9f0c13a67f7a
Hannah O'Sullivan <hjosullivan123@gmail.com>
2018-11-21 11:37:42 +0000

    Update regex.py

--- Week7/Code/regexs.py
+++ Week7/Code/regexs.py
@@ -46,3 +46,44 @@ match.group()
 
 match = re.search(r"\s\w*$", "once upon a time")
 match.group()
+
+#directly append group to result
+re.search(r"\w*\s\d.*\d", "take 2 grams of H20"). group()
+
+re.search(r"^\w*.*\s", "once upon a time"), group()
+
+#*, +, {} are all "greedy" - they repeat the previous regex
+#as many times possible
+#this may result in matching more text that you want!
+
+#to make it non-greedy use ?
+re.search(r"^\w*.*?\s", "once upon a time").group()
+
+#with an HTML tag:
+re.search(r'<.+>', 'This is a <EM>first</EM> test').group()
+
+re.search(r'<.+?>', 'This is a <EM>first</EM> test').group()
+
+re.search(r'\d*\.?\d*','1432.75+60.22i').group()
+
+re.search(r'[AGTC]+', 'the sequence ATTCGT').group()
+
+re.search(r'\s+[A-Z]\w+\s*\w+', "The bird-shit frog's name is Theloderma asper.").group()
+
+#finding email addresses in a string
+#e.g. find academic name, email and research area
+MyStr = 'Samraat Pawar, s.pawar@imperial.ac.uk, Systems biology and ecological theory'
+match = re.search(r"[\w\s]+,\s[\w\.@]+,\s[\w\s]+",MyStr)
+match.group()
+
+MyStr = 'Samraat Pawar, s-pawar@imperial.ac.uk, Systems biology and ecological theory'
+
+#does not match!
+match = re.search(r"[\w\s]+,\s[\w\.@]+,\s[\w\s&]+",MyStr)
+match.group()
+
+#make the email regex more robust
+match = re.search(r"[\w\s]+,\s[\w\.-]+@[\w\.-]+,\s[\w\s&]+",MyStr)
+match.group()
+
+#yay
