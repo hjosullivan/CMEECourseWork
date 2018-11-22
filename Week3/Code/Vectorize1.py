a76fa4bbf8a432dcfe39e5be571ae891b4b4ecb6
Hannah O'Sullivan <hjosullivan123@gmail.com>
2018-11-22 16:29:58 +0000

    Add Vectorize1.py

---
+++ Week3/Code/Vectorize1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+
+A python verison of Vectorize1.R
+
+"""
+__author__ = "Hannah O'Sullivan (h.osullivan18@imperial.ac.uk)"
+__appname__ = "Vectorize 1"
+__version__ = "0.0.1"
+__license__ = "I do not have one"
+
+
+#import modules
+import numpy as np
+
+#create matrix
+M = np.array(np.random.uniform((1000000), 1000, 1000))
+print(M)
+
+def SumAllElements(M):
+    Dimensions = np.shape(M)
+    Tot = 0
+    for i in Dimensions[0]:
+        for j in Dimensions[1]:
+            Tot = Tot + M(i,j)
+    return Tot
