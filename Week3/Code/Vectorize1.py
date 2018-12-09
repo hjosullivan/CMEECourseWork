aaf0801db58a80a4702ed1d2f1a0eb1896cb082b
hjosullivan <hjosullivan123@gmail.com>
2018-12-09 19:18:46 +0000

    Update to include function time

--- Week3/Code/Vectorize1.py
+++ Week3/Code/Vectorize1.py
@@ -1,26 +1,42 @@
 #!/usr/bin/env python3
+def main(argv=[]):
 
-"""
+    """
 
-A python verison of Vectorize1.R
+    A python verison of Vectorize1.R
 
-"""
-__author__ = "Hannah O'Sullivan (h.osullivan18@imperial.ac.uk)"
-__appname__ = "Vectorize 1"
-__version__ = "0.0.1"
-__license__ = "I do not have one"
+    """
+    __author__ = "Hannah O'Sullivan (h.osullivan18@imperial.ac.uk)"
+    __appname__ = "Vectorize 1"
+    __version__ = "0.0.1"
+    __license__ = "I do not have one"
+
+    # Import modules
+    import numpy as np
+    import time
+
+    # Define function
+    def SumAllElements(M):
+        """ A function to sum all the
+        elements in a matrix """
+        Dimensions = np.shape(M) # Get matrix dimensions
+        Tot = 0 # Initialise
+        for i in range(0, Dimensions[0]): # From the first...
+            for j in range(0, Dimensions[-1]): # to last element
+                Tot = Tot + M[i, j] # Sum these elements and add to the total
+        return Tot
+
+    # Draw random samples from a uniform distribution
+    # With the dimmensions 1000x1000
+    M = np.random.rand(1000, 1000)
+
+    # Time the function:
+    start = time.time() # Start the clock
+    SumAllElements(M) # Run function
+    # Stop the clock and print result.
+    print("The sum of all elements takes %fs to run." % (time.time() - start))
 
 
-#import modules
-import numpy as np
-
-#draw samples from a uniform distribution
-M = np.array(np.random.uniform((1000000), 1000, 1000))
-
-def SumAllElements(M):
-    Dimensions = np.shape(M)
-    Tot = 0
-    for i in Dimensions[0]:
-        for j in Dimensions[1]:
-            Tot = Tot + M(i,j)
-    return Tot
+if __name__== "__main__":
+    import sys
+    main(sys.argv)
