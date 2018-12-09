041755d3e2515813d37338dae5662122fab0b1cd
hjosullivan <hjosullivan123@gmail.com>
2018-12-09 21:53:37 +0000

    Update comments for LV1

--- Week7/Code/LV1.py
+++ Week7/Code/LV1.py
@@ -15,53 +15,55 @@ def main(argv=[]):
     __date__ = "November 2018"
     __license__ = "I do not have one"
 
-    #import packages
+    # Import packages
     import scipy as sc
     import scipy.integrate as integrate
 
-    #define function to return growth rate of consumer
-    #and resource population at any given time stepself.
+    # Define function to return growth rate of consumer
+    # and resource population at any given time steps.
 
     def dCR_dt(pops, t = 0):
         """ A function that returns growth rate
         of consumer and resource population  at
         any given time step."""
 
-        R = pops[0] #assign prey first column (list)
-        C = pops[1] #assign predator to second column (list)
+        R = pops[0] # Assign prey first column (list)
+        C = pops[1] # Assign predator to second column (list)
         dRdt = r * R - a * R * C
         dCdt = -z * C + e * a * R * C
 
         return sc.array([dRdt, dCdt])
 
-    #check type of object
+    # Check type of object
     type(dCR_dt)
 
-    #assign parameter values
-    #arbitrary but interesting!
-    r = 1.
-    a = 0.1
-    z = 1.5
-    e = 0.75
+    # Assign parameter values
+    # Arbitrary but interesting!
+    r = 1. # Instrinsic growth rate
+    a = 0.1 # Intrinsic search rate for the resource
+    z = 1.5 # Mortality rate (time-1)
+    e = 0.75 # Consumer efficiency of converting resource to biomass
 
-    #define time vector
-    #from time point 0 to 15
-    #using 1000 sub-divisions of time
-    t = sc.linspace(0, 15, 1000) #again values are arbitrary
+    # Define time vector
+    # Time point 0 to 15
+    # 1000 sub-divisions of time
+    t = sc.linspace(0, 15, 1000) # (values are arbitrary)
 
-    #set initial conditions for both populations
-    #10 resources and 5 consumers per unit area
+    # Set initial conditions for both populations
+    # 10 resources and 5 consumers per unit area
     R0 = 10
     C0 = 5
-    RC0 = sc.array([R0, C0]) #convert to array
+    RC0 = sc.array([R0, C0]) # Convert to array
 
-    #numerically integrate this system from those starting conditions
+    # Numerically integrate this system from starting conditions
     pops, infodict = integrate.odeint(dCR_dt, RC0, t,
     full_output = True)
 
-    #view array
+    # View array
+    # Contains population trajectories
     pops
 
+    # Check type of infodict
     type(infodict)
 
     infodict.keys()
@@ -74,11 +76,11 @@ def main(argv=[]):
 
     import matplotlib.pylab as p
 
-    #first plot
-    #open empty figure object
+    # First plot
+    # Open empty figure object
     f1 = p.figure()
 
-    #plot
+    # Plot
     p.plot(t, pops[:,0], "g-", label = "Resource density")
     p.plot(t, pops[:,1], "b-", label = "Consumer density")
     p.grid()
@@ -87,14 +89,15 @@ def main(argv=[]):
     p.ylabel("Population density")
     p.title("Consumer-Resource population dynamics")
 
-    #save to pdf in results directory
+    # Save to pdf in results directory
     f1.savefig("../Results/LV1_model.pdf")
 
-    #second plot
-    #plotting direction fields and trajectories in the phase plane
-    #open empty figure object
+    # Second plot
+    # Plot direction fields and trajectories in phase plane
+    # Open empty figure object
     f2 = p.figure()
 
+    # Plot
     p.plot(pops[:,0],pops[:,1],"r-")
     p.grid()
     p.legend(loc = "best")
@@ -102,7 +105,7 @@ def main(argv=[]):
     p.ylabel("Consumer density")
     p.title("Consumer-Resource population dynamics")
 
-    #save to pdf in results directory
+    # Save to pdf in results directory
     f2.savefig("../Results/LV1_phase.pdf")
 
 if __name__== "__main__":
