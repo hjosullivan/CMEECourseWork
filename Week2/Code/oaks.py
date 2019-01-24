5352fada76503d3b00d6e2aaca7ef07a6d7f2aca
Hannah O'Sullivan <hjosullivan123@gmail.com>
2019-01-24 12:06:15 +0000

    Testing pycharm with oaks.py

--- Week2/Code/oaks.py
+++ Week2/Code/oaks.py
@@ -12,18 +12,19 @@ __appname__ = "Oaks"
 __version__ = "0.0.1"
 __license__ = "I do not have one"
 
-
 ## Finds just those taxa that are oak trees from a list of species
 
-taxa = [ 'Quercus robur',
-          'Fraxinus excelsior',
-          'Pinus sylvestris',
-          'Quercus cerris',
-          'Quercus petraea'
-          ]
+taxa = ['Quercus robur',
+        'Fraxinus excelsior',
+        'Pinus sylvestris',
+        'Quercus cerris',
+        'Quercus petraea'
+        ]
+
 
 def is_an_oak(name):
     return name.lower().startswith('quercus ')
+
 
 ## Using for loops
 
@@ -31,11 +32,10 @@ oaks_loops = set()
 for species in taxa:
     if is_an_oak(species):
         oaks_loops.add(species)
-
 print(oaks_loops)
 
 ## Using list comprehensions
-oaks_lc = set([species for species in taxa if is_an_oak (species)])
+oaks_lc = set([species for species in taxa if is_an_oak(species)])
 print(oaks_lc)
 
 ## Get names in UPPER CASE using for loops
@@ -45,6 +45,6 @@ for species in taxa:
         oaks_loops.add(species.upper())
 print(oaks_loops)
 
-#get names in upper case using list comprehensions
+# get names in upper case using list comprehensions
 oaks_lc = set([species.upper() for species in taxa if is_an_oak(species)])
 print(oaks_lc)
